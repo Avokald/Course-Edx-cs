7a1e1cd48d995ecf14262fb9f800dfb75e25ac97
Baldykov Aibek <baldykov_a@yahoo.com>
2017-02-22 23:05:00 +0600

    vigenere.py
    vigenere cipher python

---
+++ other/cipher.py
@@ -0,0 +1,80 @@
+def decrypt(pltext, key):
+    #key = str(input("Keyword: "))
+    keylen = len(key)
+    #print("Plaintext: ", end="")
+    # = str(input())
+    j = 0
+    encText = ""
+    for i in pltext:
+        if i.isalpha():
+            tempKeyChar = key[j]
+            if i.isupper():
+                if tempKeyChar.isupper():
+                    encText += chr(65 + (ord(i) + 13 - (ord(tempKeyChar)+ 13) ) %26 )
+                elif tempKeyChar.islower():
+                    encText += chr(65 + (ord(i) + 13 - (ord(tempKeyChar)+ 7) ) %26 )
+                else:
+                    raise KeyError("Error")
+            elif i.islower():
+                if tempKeyChar.isupper():
+                    encText += chr(97 + (ord(i) + 7 - (ord(tempKeyChar)+ 13) ) %26 )
+                elif tempKeyChar.islower():
+                    encText += chr(97 + (ord(i) + 7 - (ord(tempKeyChar)+ 7) ) %26 )
+                else:
+                    raise KeyError("ERROR")
+            else:
+                print("Error")
+                quit(2)
+        else:
+            print("Error")
+            quit(1)
+    
+        j = (j + 1)%keylen
+    print("Result:", encText)
+    return encText
+
+def encrypt(pltext, key):
+    #key = str(input("Keyword: "))
+    keylen = len(key)
+    
+    #print("Plaintext: ", end="")
+    #pltext = str(input())
+    j = 0
+    encText = ""
+    for i in pltext:
+        if i.isalpha():
+            tempKeyChar = key[j]
+            if i.isupper():
+                if tempKeyChar.isupper():
+                    encText += chr(65 + (ord(i) + 13 + (ord(tempKeyChar)+ 13) ) %26 )
+                elif tempKeyChar.islower():
+                    encText += chr(65 + (ord(i) + 13 + (ord(tempKeyChar)+ 7) ) %26 )
+                else:
+                    raise KeyError("Error")
+            elif i.islower():
+                if tempKeyChar.isupper():
+                    encText += chr(97 + (ord(i) + 7 + (ord(tempKeyChar)+ 13) ) %26 )
+                elif tempKeyChar.islower():
+                    encText += chr(97 + (ord(i) + 7 + (ord(tempKeyChar)+ 7) ) %26 )
+                else:
+                    raise KeyError("ERROR")
+            else:
+                print("Error")
+                quit(2)
+        else:
+            print("Error")
+            quit(1)
+    
+        j = (j + 1)%keylen
+    print("Result:", encText)
+    return encText
+    
+    
+    
+def main():
+    text = str(input("Text: "))
+    key = str(input("Keyword: "))
+    encText = encrypt(text, key)
+    decrypt(encText, key)
+if __name__ == "__main__":
+    main()
